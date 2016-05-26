1d249a19d09816c38b1949af3f7a25493a4965f9
Jake Albaugh <jake.albaugh@gmail.com>
2016-05-26 14:18:45 -0500

    emoji slack support

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -29,7 +29,7 @@ class NotesController < ApplicationController
 
     text = slack_note_params[:text]
     text = text.gsub('notey ', '')
-    text = Filter::Emoji.new(text).text
+    text = Filters::Emoji.new(text).text
     color = text.match(/\A#[0-9a-fA-Z]+ /)
     if color
       text = text.gsub(color[0], '')
