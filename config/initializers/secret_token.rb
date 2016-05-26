ea40fa8b182a49d6fd3970f245ddafa4a251c3e9
Jake Albaugh <jake.albaugh@gmail.com>
2016-05-26 10:20:40 -0500

    renames to notey

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Jakenote::Application.config.secret_token = '074b1e689b70a7f9263343c729f486f428658c8f2c4ca6658020000760bc90117207ca0ab6a1ce86a929a42e6e31a84ffe7bf40f5e12734c4e30d9b562843e51'
+Notey::Application.config.secret_token = '074b1e689b70a7f9263343c729f486f428658c8f2c4ca6658020000760bc90117207ca0ab6a1ce86a929a42e6e31a84ffe7bf40f5e12734c4e30d9b562843e51'
