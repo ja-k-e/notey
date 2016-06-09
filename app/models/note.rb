edf0d27f6008ec7e8dcb99871909b420f7106c4a
Jake Albaugh <jake.albaugh@gmail.com>
2016-06-09 11:40:00 -0500

    message optional

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :user, inverse_of: :notes
 
-  validates :message, presence: true
   validates :color, format: /\A#[a-fA-F0-9]{3}([a-fA-F0-9]{3})?\z/, allow_blank: true
 
   before_save :default_values
